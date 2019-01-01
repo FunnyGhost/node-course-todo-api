5954085cb628b58a206d91085b8dd8428da0a557
Catalin Ciubotaru <catalinciubot@gmail.com>
2019-01-01 23:54:46 +0100

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,17 @@ app.post('/todos', (req, res) => {
   );
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then(
+    todos => {
+      res.send({ todos });
+    },
+    err => {
+      res.status(400).send(err);
+    }
+  );
+});
+
 app.listen(43222, () => {
   console.log('Started on port 43222');
 });
