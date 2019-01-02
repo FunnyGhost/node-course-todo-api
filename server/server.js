531350d8f07c75e258ea5bf329f5366eba39f5bf
Catalin Ciubotaru <catalinciubot@gmail.com>
2019-01-02 17:43:51 +0100

    Add route for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { User } = require('./models/user');
@@ -15,6 +16,27 @@ app.post('/todos', (req, res) => {
   todo.save().then(
     doc => {
       res.send(doc);
+    },
+    err => {
+      res.status(400).send(err);
+    }
+  );
+});
+
+app.get('/todos/:id', (req, res) => {
+  const todoId = req.params.id;
+
+  if (!ObjectID.isValid(todoId)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(todoId).then(
+    todo => {
+      if (todo) {
+        res.send({ todo });
+      } else {
+        res.status(404).send();
+      }
     },
     err => {
       res.status(400).send(err);
