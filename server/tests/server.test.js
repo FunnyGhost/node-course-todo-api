8dc4bc873dd1a363d8bd6a80b34067487a7c79b5
Catalin Ciubotaru <catalinciubot@gmail.com>
2019-01-02 17:57:06 +0100

    Add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,20 +1,23 @@
 const expect = require('expect');
 const request = require('supertest');
+const { ObjectID } = require('mongodb');
 
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
 const todos = [
   {
+    _id: new ObjectID(),
     text: 'First test todo'
   },
   {
+    _id: new ObjectID(),
     text: 'Second test todo'
   }
 ];
 
 beforeEach(done => {
-  Todo.remove({})
+  Todo.deleteMany({})
     .then(() => {
       return Todo.insertMany(todos);
     })
@@ -75,6 +78,34 @@ describe('GET /todos', () => {
       .expect(res => {
         expect(res.body.todos.length).toBe(2);
       })
+      .end(done);
+  });
+});
+
+describe('GET /todos/:id', () => {
+  it('should return todo doc', done => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return 404 if todo not found', done => {
+    const validButNotExistentId = new ObjectID().toHexString();
+    request(app)
+      .get(`/todos/${validButNotExistentId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if id not valid', done => {
+    const invalidId = '123';
+    request(app)
+      .get(`/todos/${invalidId}`)
+      .expect(404)
       .end(done);
   });
 });
