a9916948448f05ebf9cc62b4debe6ac7eeb9c41c
Catalin Ciubotaru <catalinciubot@gmail.com>
2019-01-04 14:17:15 +0100

    Added environment setup

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
+const mongoURI = process.env.MONGODB_URI;
 mongoose.connect(
   mongoURI,
   { useNewUrlParser: true }
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const express = require('express');
 const _ = require('lodash');
 const bodyParser = require('body-parser');
@@ -8,7 +10,7 @@ const { User } = require('./models/user');
 const { Todo } = require('./models/todo');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
