4102351d83eee934f886fb3f9f12d4dd7f669a19
Catalin Ciubotaru <catalinciubot@gmail.com>
2019-01-07 17:05:43 +0100

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,13 +1,30 @@
 require('colors');
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-const data = {
-  id: 10
-};
+const password = '123abc!';
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+  console.log(salt.white);
+  console.log(hash.green);
+  });
+});
 
-const token = jwt.sign(data, '123abc');
-console.log(`Token is ${token}`.yellow);
+// const hashedPassword = '$2a$10$Bt2hD0ZhzVFgur3dQVNPBOcoCzrxKOkhxpK5YqL6IeB/BmOa9J4La';
+// bcrypt.compare(password, hashedPassword, (err, res) => {
+//   console.log(res);
+// });
 
-const decoded = jwt.verify(token, '123abc');
-console.log(`Decoded is ${JSON.stringify(decoded)}`.green);
+
+// const data = {
+//   id: 10
+// };
+
+// const token = jwt.sign(data, '123abc');
+// console.log(`Token is ${token}`.yellow);
+
+// const decoded = jwt.verify(token, '123abc');
+// console.log(`Decoded is ${JSON.stringify(decoded)}`.green);
+
+
